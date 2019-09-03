bc8b5faa59c41d87465e8b22d7d837e730b16371
Vicenzo <vicenzo@sponte.com.br>
2019-09-03 14:43:03 -0300

    initial

---
+++ Models/SearchResult.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ViaCepConsumer.Models
+{
+    public class SearchResult
+    {
+        [JsonProperty("cep")]
+        public string Cep { get; set; }
+
+        [JsonProperty("logradouro")]
+        public string Rua { get; set; }
+
+        [JsonProperty("complemento")]
+        public string Complemento { get; set; }
+
+        [JsonProperty("bairro")]
+        public string Bairro { get; set; }
+
+        [JsonProperty("localidade")]
+        public string Cidade { get; set; }
+
+        [JsonProperty("uf")]
+        public string EstadoUf { get; set; }
+
+        [JsonProperty("unidade")]
+        public string Unidade { get; set; }
+
+        [JsonProperty("ibge")]
+        public int CodigoIbge { get; set; }
+
+        [JsonProperty("gia")]
+        public int Gia { get; set; }
+    }
+}
---
+++ ViaCepConsumer.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using ViaCepConsumer.Models;
+
+namespace ViaCepConsumer
+{
+    public class ViaCepConsumer
+    {
+        private const string BaseUrl = "https://viacep.com.br";
+
+        public SearchResult Search(string zipCode) => SearchAsync(zipCode, CancellationToken.None).GetAwaiter().GetResult();
+
+        public async Task<SearchResult> SearchAsync(string zipCode, CancellationToken token)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                var response       = await client.GetAsync($"/ws/{zipCode}/json", token).ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
+                var @return        = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<SearchResult>(@return);
+            }
+        }
+
+        public IEnumerable<SearchResult> Search(
+            string estado,
+            string cidade,
+            string endereco) 
+            => SearchAsync(estado, cidade, endereco, CancellationToken.None).GetAwaiter().GetResult();
+
+        public async Task<IEnumerable<SearchResult>> SearchAsync(
+            string estado,
+            string cidade,
+            string endereco,
+            CancellationToken token)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri(BaseUrl);
+                var response       = await client.GetAsync($"/ws/{estado}/{cidade}/{endereco}/json", token).ConfigureAwait(false);
+                response.EnsureSuccessStatusCode();
+
+                var @return        = await response.Content.ReadAsStringAsync();
+
+                return JsonConvert.DeserializeObject<IEnumerable<SearchResult>>(@return);
+            }
+        }
+    }
+}
